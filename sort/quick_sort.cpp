d2c6f885347ee308137ddd5c678b10322fef75e7
cheny <dream.cy@foxmail.com>
2020-09-06 01:21:39 +0800

    修改 快速排序算法

--- sort/quick_sort.cpp
+++ sort/quick_sort.cpp
@@ -5,22 +5,22 @@ void quick_sort(int array[], int first, int last) {
         return;
     }
     int l = first, r = last;
-    int pivot = array[first + (last - first) / 2];
+    int pivot = array[first + (last - first + 1) / 2];
     while (true) {
         while (array[l] < pivot) ++l;
         while (array[r] > pivot) --r;
-        if (l >= r) break;
+        if (!(l < r)) break;
         int tmp = array[l];
         array[l] = array[r];
         array[r] = tmp;
         ++l, --r;
     }
     quick_sort(array, first, l - 1);
-    quick_sort(array, l + 1, last);
+    quick_sort(array, l, last);
 }
 
 int main(int argc, char* argv[]) {
-    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int array[] = {0, 16, 5, 6, 11, 7, 9, 1, 8, 4, 19, 17, 3, 18, 10, 14, 12, 15, 13, 2};
     int size = sizeof(array) / sizeof(array[0]);
     quick_sort(array, 0, size - 1);
     for (int i = 0; i < size; ++i) {
